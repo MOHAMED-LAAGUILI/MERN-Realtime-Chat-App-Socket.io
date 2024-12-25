b8ec5466e6b49d3ade6ebb8365a824e7373ae25c
MOHAMED-LAAGUILI-vnb <94993660+MOHAMED-LAAGUILI@users.noreply.github.com>
2024-12-25 18:50:49 +0100

    dd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,47 +1,52 @@
-import { Navigate, Route, Routes } from "react-router-dom";
-import { useEffect } from "react";
-import NavBar from "./components/NavBar";
+import Navbar from "./components/Navbar";
+
 import HomePage from "./pages/HomePage";
-import SignupPage from "./pages/SignupPage";
-import SigninPage from "./pages/SigninPage";
-import ProfilePage from "./pages/ProfilePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
+
+import { Routes, Route, Navigate } from "react-router-dom";
 import { useAuthStore } from "./store/useAuthStore";
-import { ripples } from "ldrs";
+import { useThemeStore } from "./store/useThemeStore";
+import { useEffect } from "react";
 
-function App() {
-  const { user, CheckAuth, isCheckingAuth } = useAuthStore();
+import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
 
-  // Register the ldrs ripple effect
-  ripples.register();
+const App = () => {
+  const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
+  const { theme } = useThemeStore();
 
-  // Check user authentication status on mount
+  console.log({ onlineUsers });
+
   useEffect(() => {
-    CheckAuth();
-  }, [CheckAuth]);
+    checkAuth();
+  }, [checkAuth]);
 
-  // Render a loader while checking authentication
-  if (isCheckingAuth && !user) {
+  console.log({ authUser });
+
+  if (isCheckingAuth && !authUser)
     return (
       <div className="flex items-center justify-center h-screen">
-        <l-ripples size="60" speed="2" color="black" />
+        <Loader className="size-10 animate-spin" />
       </div>
     );
-  }
 
-  // Main App layout
   return (
-    <>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={user ? <HomePage /> : <Navigate to="/signin" />} />
-        <Route path="/signup" element={!user ?  <SignupPage /> :  <Navigate to="/" />} />
-        <Route path="/signin" element={!user ? <SigninPage /> : <Navigate to="/" />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/settings" element={user ? <SettingsPage /> : <Navigate to="/signin" />} />
-      </Routes>
-    </>
-  );
-}
+    <div data-theme={theme}>
+      <Navbar />
 
+      <Routes>
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
+      </Routes>
+
+      <Toaster />
+    </div>
+  );
+};
 export default App;
