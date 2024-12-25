2ed8dbe5e72df593bd628129875d4ee86ff0756c
MOHAMED-LAAGUILI-vnb <94993660+MOHAMED-LAAGUILI@users.noreply.github.com>
2024-12-25 20:48:57 +0100

    hh

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,7 +2,13 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const MONGO_URI="mongodb+srv://mohamed:mohamed2025@cluster0.yojmo.mongodb.net/chatapp?retryWrites=true&w=majority&appName=Cluster0";
+    if (!MONGO_URI) {
+      console.error("MongoDB URI is not defined!");
+      process.exit(1);  // Exit if no URI is found
+    }
+    
+    const conn = await mongoose.connect(MONGO_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.log("MongoDB connection error:", error);
